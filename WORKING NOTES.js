d74dff97b4dcac05978a4c3b4c8c6df9859d8012
Hong Pham <11528891+hongpham@users.noreply.github.com>
2020-01-23 09:45:24 -0800

    Delete WORKING NOTES.js

--- WORKING NOTES.js
+++
@@ -1,9 +0,0 @@
-WORKING NOTE
-
-
------
-Create javascript zip file to upload S3
-zip -vr ../createKeyPair.zip .
-
-validate template
-aws cloudformation validate-template --template-body file://provision_devices.template
